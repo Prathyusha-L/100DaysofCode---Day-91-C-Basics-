b08944784728ba01d8724bfc8d89472137c85188
L Prathyusha <83442712+Prathyusha-L@users.noreply.github.com>
2021-09-24 07:01:45 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
+
+
+// Array declaration by initializing elements
+int arr[] = { 10, 20, 30, 40 }
+
+// Compiler creates an array of size 4.
+// above is same as "int arr[4] = {10, 20, 30, 40}"
